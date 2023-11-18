7912af39418d976c4a690b828f68551c311fb42e
artificialLeo <horkovenko.k@gmail.com>
2023-11-18 20:18:26 +0200

    -transactional, pom edit.

--- store/src/main/java/com/book/store/repo/impl/BookRepositoryImpl.java
+++ store/src/main/java/com/book/store/repo/impl/BookRepositoryImpl.java
@@ -4,10 +4,8 @@ import com.book.store.model.Book;
 import com.book.store.repo.BookRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
-@Transactional
 public interface BookRepositoryImpl extends JpaRepository<Book, Long>, BookRepository {
 
 }
