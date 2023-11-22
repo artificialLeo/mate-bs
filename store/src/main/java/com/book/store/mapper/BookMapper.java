289a7042342f57e7fade0d79571fb202855c74c1
khorkovenko <khorkovenko>
2023-11-22 17:40:43 +0200

    added map conf

--- store/src/main/java/com/book/store/mapper/BookMapper.java
+++ store/src/main/java/com/book/store/mapper/BookMapper.java
@@ -4,8 +4,10 @@ import com.book.store.dto.BookDto;
 import com.book.store.dto.BookRequestDto;
 import com.book.store.model.Book;
 import org.mapstruct.Mapper;
+import org.mapstruct.MapperConfig;
 
 @Mapper
+@MapperConfig(componentModel = "spring")
 public interface BookMapper {
     BookDto toBookDto(Book book);
 
